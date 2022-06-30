9d4445b983ccb8e7bcd519f5633c5b9ffd769a8c
p9romain <romainperrod.pro9@gmail.com>
2022-06-30 19:17:21 +0200

    Update

--- main.cpp
+++ main.cpp
@@ -2,6 +2,25 @@
 #include <vector>
 #include <conio.h>
 
+#define KEY_UP 72
+#define KEY_DOWN 80
+#define KEY_LEFT 75
+#define KEY_RIGHT 77
+#define ESC 27
+
+#define KEY_Z 90
+#define KEY_S 83
+#define KEY_Q 81
+#define KEY_D 68
+#define KEY_X 88
+
+#define KEY_z 122
+#define KEY_s 115
+#define KEY_q 113
+#define KEY_d 100
+#define KEY_x 120
+
+
 bool gameOver ;
 // height x width
 const std::vector<int> sizeGrid = std::vector<int>{{ 30, 60 }} ;
@@ -66,25 +85,37 @@ void input()
 {
   if ( _kbhit() )
   {
-    switch ( _getch() )
+    switch ( getch() )
     {
-      case 'z' : 
+      case KEY_Z :
+      case KEY_z :
+      case KEY_UP : 
         dir = DIRECTION::UP ;
         break ;
 
-      case 'q' : 
+      case KEY_Q :
+      case KEY_q :
+      case KEY_LEFT : 
         dir = DIRECTION::LEFT ;
         break ;
 
-      case 's' : 
+      case KEY_S :
+      case KEY_s :
+      case KEY_DOWN :
         dir = DIRECTION::DOWN ;
         break ;
 
-      case 'd' : 
-        dir = DIRECTION::LEFT ;
+      case KEY_D :
+      case KEY_d :
+      case KEY_RIGHT :
+        dir = DIRECTION::RIGHT ;
         break ;
 
-      case ''
+      case KEY_X :
+      case KEY_x :
+      case ESC :
+        gameOver = true ;
+        break ;
     }
   }
 }
